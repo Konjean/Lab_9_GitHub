257ec08f775f579b2f25289f3e31984e85ca9117
Vegasa620-th <parin289@hotmail.com>
2024-09-10 14:17:36 +0700

    update feature1

--- Lab_9_GitHub/Game1.cs
+++ Lab_9_GitHub/Game1.cs
@@ -8,7 +8,7 @@ namespace Lab_9_GitHub
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        bool isHit;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
